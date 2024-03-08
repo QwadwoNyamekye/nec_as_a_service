14b9a8a05897517db20d944d1970572905bd8d65
OPPONG-NKRUMAH David <DOPPONG-NKRUMAH@ghipss.com>
2024-03-08 12:09:01 +0000

    added countdown to redirect on password change success

--- src/app/pages/auth/change-password/change-password.component.ts
+++ src/app/pages/auth/change-password/change-password.component.ts
@@ -1,19 +1,17 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   Inject,
+  OnInit,
 } from "@angular/core";
 import { Router } from "@angular/router";
 import { NB_AUTH_OPTIONS } from "@nebular/auth";
 import { getDeepFromObject } from "@nebular/auth";
 import { NbAuthService } from "@nebular/auth";
 import { NecService } from "../../../@core/mock/nec.service";
+import { timer } from "rxjs";
+import { takeWhile, map } from "rxjs/operators";
 
 @Component({
   selector: "nb-reset-password-page",
@@ -21,7 +19,7 @@ import { NecService } from "../../../@core/mock/nec.service";
   templateUrl: "./change-password.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChangePassword {
+export class ChangePassword implements OnInit {
   redirectDelay: number = 0;
   showMessages: any = {};
   strategy: string = "";
@@ -31,19 +29,54 @@ export class ChangePassword {
   messages: string[] = [];
   user: any = {};
   new_user_credentials: any = {};
+  passwordRegex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+  passwordRegexInvalid: boolean;
+  countdownMessage: string;
 
   constructor(
     protected service: NbAuthService,
     protected necService: NecService,
     @Inject(NB_AUTH_OPTIONS) protected options = {},
     protected cd: ChangeDetectorRef,
-    protected router: Router
+    public router: Router
   ) {
     this.redirectDelay = this.getConfigValue(
       "forms.resetPassword.redirectDelay"
     );
     this.showMessages = this.getConfigValue("forms.resetPassword.showMessages");
     this.strategy = this.getConfigValue("forms.resetPassword.strategy");
+  }
+  ngOnInit(): void {
+    this.passwordRegexInvalid = true;
+  }
+
+  _console(v) {
+    console.log(v);
+  }
+  validatePassword(password) {
+    console.log("YYYYYYYYYYYYYYYYYYYY");
+    if (this.passwordRegex.test(password)) {
+      console.log("Password is valid!");
+      this.passwordRegexInvalid = false;
+      return "Password is Valid";
+    } else {
+      console.log(this.passwordRegex.test(password));
+      console.log("Password is invalid. Reasons:");
+      if (!/[a-z]/.test(password)) {
+        console.log("- Password must contain at least one lowercase letter.");
+        return "Password must contain at least one lowercase letter.";
+      } else if (!/[A-Z]/.test(password)) {
+        console.log("- Password must contain at least one uppercase letter.");
+        return "Password must contain at least one uppercase letter.";
+      } else if (!/[0-9]/.test(password)) {
+        console.log("- Password must contain at least one digit.");
+        return "Password must contain at least one digit.";
+      } else if (!/[!@#$%^&*_=+-]/.test(password)) {
+        console.log("- Password must contain at least one special character.");
+        return "Password must contain at least one special character.";
+      }
+    }
   }
 
   resetPass(): void {
@@ -66,7 +99,16 @@ export class ChangePassword {
         this.submitted = false;
         if (result.errorCode == "0") {
           this.messages[0] = result.errorMessage;
-          return this.router.navigate(["auth/login"]);
+
+          var time = 5;
+          setInterval(function (router) {
+            var seconds = time;
+            document.getElementById("time").innerHTML = "Redirecting to Login in : " + seconds;
+            if (time == 0) {
+              router.navigate(["auth/login"]);
+            }
+            time--
+          }, 1000, this.router);
         } else if (result.errorCode == "1") {
           this.errors[0] = result.errorMessage;
         }
